45bc5db557b90b4a1c806f6ca464eda49ddb4fae
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-11-07 20:55:56 -0800

    Added test cases for boardgame

--- classes.test.js
+++ classes.test.js
@@ -1,4 +1,5 @@
-import { Ship } from "./classes"
+/* eslint-disable no-undef */
+import { Ship, Gameboard } from "./classes"
 
 describe("Ship class", () => {
  let length = 5 
@@ -31,7 +32,83 @@ describe("Ship class", () => {
 })
 
 describe("Gameboard class", () => {
+   let gameboard
+   let ship
 
+   beforeEach(() => {
+      gameboard = new Gameboard()
+      ship = new Ship(4)
+   })
+
+   it.skip("Invalid place due to vertical bounds", () => {
+      expect(() => {
+         gameboard.addShip(ship, [7,2])
+      }).toThrow()
+   })
+
+   it.skip("Invalid place due to horizontal bounds", () => {
+      expect(() => {
+         gameboard.addShip(ship, [0,8], false)
+      }).toThrow()
+   })
+
+   it.skip("Invalid place to put due to ship in way", () => {
+      gameboard.addShip(ship, [3,0])
+      const newShip = new Ship(3)
+      expect(() => {
+         gameboard.addShip(newShip, [2,1], false)
+      }).toThrow()
+   })
+
+   it.skip("Valid place to put vertical ship", () => {
+      expect(gameboard.addShip(ship, [0,0])).toBe(true)
+   })
+
+   it.skip("Valid place to put horizontal ship", () => {
+      expect(gameboard.addShip(ship, [0,0], false)).toBe(true)
+   })
+
+   describe("Actions in game", () => {
+      beforeEach(() => {
+         gameboard.addShip(ship, [0,0])
+      })
+
+      it.skip("Acknowledges a hit", () => {
+         expect(gameboard.receiveAttack([0,0])).toBe(true)
+      })
+
+      it.skip("Acknowledges a miss", () => {
+         expect(gameboard.receiveAttack([1,0])).toBe(false)
+      })
+
+      it.skip("Acknowledges a wipeout", () => {
+         for (let i = 0; i < 4; i += 1) {
+            gameboard.receiveAttack([i,0])
+         }
+         expect(gameboard.isWiped()).toBe(true)
+      })
+
+      it.skip("Acknowledges a continued game", () => {
+         for (let i = 0; i < 3; i += 1) {
+            gameboard.receiveAttack([i,0])
+         }
+         expect(gameboard.isWiped()).toBe(false)
+      })
+
+      it.skip("Prevents repeated hit", () => {
+         gameboard.receiveAttack([0,0])
+         expect(() => {
+            gameboard.receiveAttack([0,0])
+         }).toThrow()
+      })
+
+      it.skip("Prevents repeated miss", () => {
+         gameboard.receiveAttack([0,1])
+         expect(() => {
+            gameboard.receiveAttack([0,1])
+         }).toThrow()
+      })
+   })
 })
 
 describe("Player class", () => {
