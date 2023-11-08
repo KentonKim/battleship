1eeed8e11d0d4da747c76bfa6f315280bbb183af
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-11-07 20:45:50 -0800

    modified by prettier

--- classes.js
+++ classes.js
@@ -1,56 +1,52 @@
 export class Ship {
-    constructor(length) {
-        this.length = length
-        this.hitCount = 0 
-        this._isSunk = false
-    }
+  constructor(length) {
+    this.length = length;
+    this.hitCount = 0;
+    this._isSunk = false;
+  }
 
-    hit() {
-        this.hitCount += 1
-    }
+  hit() {
+    this.hitCount += 1;
+  }
 
-    isSunk() {
-        if (this.hitCount === this.length) {
-            return true
-        } else if (this.hitCount > this.length) {
-            throw new Error('Ship has been hit more than its length')
-        }
-        return false
+  isSunk() {
+    if (this.hitCount === this.length) {
+      return true;
+    } else if (this.hitCount > this.length) {
+      throw new Error("Ship has been hit more than its length");
     }
+    return false;
+  }
 }
 
 export class Gameboard {
-    constructor() {
-        this.ships = []
-        this.log = [] // keep track of missed attacks
+  constructor() {
+    this.log = []; // keep track of missed attacks
+    this._shipLocations = {};
+  }
 
-    }
+  addShip(ship, coordinates, isVertical = true) {}
 
-    addShip(ship) {
+  deleteShip() {}
 
-    }
+  moveShip(ship) {
+    
+  }
 
-    moveShip(ship) {
+  receiveAttack(coordinates) {
+    // sends hit to correct ship
+    // or records coords of missed shot
+  }
 
-    }
-
-    receiveAttack(coordinates) {
-        // sends hit to correct ship
-        // or records coords of missed shot
-    }
-
-    isWiped() {
-        //  report if all ships have been sunk
-    }
+  isWiped() {
+    //  report if all ships have been sunk
+  }
 }
 
 export class Player {
-    constructor() {
-    }
+  constructor() {}
 }
 
 export class Computer extends Player {
-    constructor() {
-
-    }
+  constructor() {}
 }
