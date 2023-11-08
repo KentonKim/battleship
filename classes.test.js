e3a591c93e60ad469980f13f1c1a2f1013703d08
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-11-08 14:12:09 -0800

    Added tests for computer moves and battlelog

--- classes.test.js
+++ classes.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { Ship, Gameboard } from "./classes"
+import { Ship, Gameboard, Computer, Battlelog, Player } from "./classes"
 
 describe("Ship class", () => {
  let length = 5 
@@ -71,6 +71,17 @@ describe("Gameboard class", () => {
       }).toThrow()
    })
 
+   it.skip("Invalid place to put due to ship nearby", () => {
+      gameboard.addShip(ship, [0,3])
+      const newShip = new Ship(2, false)
+      expect(() => {
+         gameboard.addShip(newShip, [2,1])
+      }).toThrow()
+      expect(() => {
+         gameboard.addShip(newShip, [4,4])
+      }).toThrow()
+   })
+
    it("Valid place to put vertical ship", () => {
       expect(gameboard.addShip(ship, [0,0])).toBe(true)
    })
@@ -128,5 +139,118 @@ describe("Player class", () => {
 })
 
 describe("Computer class", () => {
+   let computer 
+   beforeEach(() => {
+      computer = new Computer('Comp')
+   })
+
+   it("Allows for changing difficulty", () => {
+      expect(() => {
+        computer.difficulty = 0 
+      }).not.toThrow()
+   })
+
+   it("Prevents incorrectly changing difficulty", () => {
+      expect(() => {
+         computer.difficulty = 10
+      }).toThrow()
+   })
+
+   describe("Computer moves", () => {
+      // Initialize a board
+
+      describe("Easy computer moves", () => {
+         beforeAll(() => {
+            computer.difficulty = 0
+         })
+
+         it.skip("Easy computer attacks random place", () => {
+
+         })
+
+         it.skip("Easy computer attacks 25% place", () => {
+
+         })
+
+         it.skip("Easy computer attacks 25% place", () => {
+
+         })
+      })
+
+      describe("Med computer moves", () => {
+         beforeAll(() => {
+            computer.difficulty = 1
+         })
+         it.skip("Easy computer attacks calculated place", () => {
+
+         })
+
+      })
+
+      describe("Hard computer moves", () => {
+         beforeAll(() => {
+            computer.difficulty = 2
+         })
+         it.skip("Hard computer attacks probable place", () => {
+
+         })
+
+      })
+   })
+})
+
+describe("Battlelog class", () => {
+   let battlelog
+   let player1
+   let player2
+
+   beforeAll(() => {
+      player1 = new Player('user')
+      player2 = new Player('computer')
+      battlelog = new Battlelog()
+      battlelog.addLog(2, player1, [0,0])
+      battlelog.addLog(2, player2, [0,1])
+      battlelog.addLog(3, player1, [0,2])
+   })
+
+   it("Displays log", () => {
+      expect(battlelog.log).toEqual([
+         {
+            turn: 1,
+            player: player1,
+            result: 2,
+            coordinates: [0,0]
+         },
+         {
+            turn: 2,
+            player: player2,
+            result: 2,
+            coordinates: [0,1]
+         },
+         {
+            turn: 3,
+            player: player1,
+            result: 3,
+            coordinates: [0,2]
+         }
+      ])
+   })
+
+   it("Displays filtered log", () => {
+      expect(battlelog.filteredLog(player1)).toEqual([
+         {
+            turn: 1,
+            player: player1,
+            result: 2,
+            coordinates: [0,0]
+         },
+         {
+            turn: 3,
+            player: player1,
+            result: 3,
+            coordinates: [0,2]
+         }
+      ])
+   })
 })
 
